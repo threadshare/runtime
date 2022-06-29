fa981ed5005625ac2aa0d6d0196f1895e71d9270
GitHub Action <action@github.com>
2022-06-29 07:40:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/threadshare/runtime/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/threadshare/runtime/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.threadshare.runtime.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.threadshare.runtime.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/threadshare/runtime/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/threadshare/runtime/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.threadshare.runtime.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.threadshare.runtime.MyBundle
 
 class MyApplicationService {
 
